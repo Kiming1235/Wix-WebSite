46a351e1a63dbf1ce0aadadf10338c050cb215e9
Wix Remote Machine <support@wix.com>
2025-11-24 08:31:52 +0000

    Add Naver verification meta tag to head

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -4,6 +4,7 @@ export const Head = () => {
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="최적화된 맞춤형 운송 솔루션과 안전하고 효율적인 작업을 통해 고객의 성공적인 프로젝트 완수를 지원합니다." />
+      <meta name="naver-site-verification" content="4775914a9f3ec89f8319d68d878db78083e33fc2" />
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
     </>
