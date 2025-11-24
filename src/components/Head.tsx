f4cc765c186f6085585a419bec1b7047c37bfb1e
Wix Remote Machine <support@wix.com>
2025-11-24 04:33:17 +0000

    Add Open Graph description for site

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -4,6 +4,7 @@ export const Head = () => {
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="최적화된 맞춤형 운송 솔루션과 안전하고 효율적인 작업을 통해 고객의 성공적인 프로젝트 완수를 지원합니다." />
+      <meta property="og:description" content="최적화된 맞춤형 운송 솔루션과 안전하고 효율적인 작업을 통해 고객의 성공적인 프로젝트 완수를 지원합니다." />
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
     </>
