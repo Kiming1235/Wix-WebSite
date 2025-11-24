128b9f588222ef9b96448295a276dbf44fd7791e
Wix Remote Machine <support@wix.com>
2025-11-24 00:26:21 +0000

    Manual edits

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -99,12 +99,10 @@ export default function Layout() {
           </AnimatePresence>
         </nav>
       </header>
-
       {/* Main Content */}
       <main className="pt-20">
         <Outlet />
       </main>
-
       {/* Footer */}
       <footer className="bg-dark-gray border-t border-secondary/20 py-16 px-6">
         <div className="max-w-[100rem] mx-auto">
@@ -144,20 +142,9 @@ export default function Layout() {
               <div className="space-y-2 font-paragraph text-secondary-foreground">
                 <p>
                   <span className="text-foreground">전화:</span>{' '}
-                  <a href="tel:010-0000-0000" className="text-primary hover:text-primary/80">
-                    010-0000-0000
-                  </a>
+                  <a href="tel:010-0000-0000" className="text-primary hover:text-primary/80">{"010-2332-4822"}</a>
                 </p>
-                <p>
-                  <span className="text-foreground">이메일:</span>{' '}
-                  <a href="mailto:info@daehancargo.co.kr" className="text-primary hover:text-primary/80">
-                    info@daehancargo.co.kr
-                  </a>
-                </p>
-                <p>
-                  <span className="text-foreground">주소:</span><br />
-                  서울특별시 강남구 테헤란로 123
-                </p>
+
               </div>
             </div>
           </div>
