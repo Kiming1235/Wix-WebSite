efa32fe8e9a64143160dc7e4a0fdc14715ee340a
Wix Remote Machine <support@wix.com>
2025-11-25 04:25:25 +0000

    Update homepage title to 대한카고크레인 | 중량물·건설자재·화물 운송 전문

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,7 +1,7 @@
 export const Head = () => {
   return (
     <>
-      <title>대한카고크레인 | 중량물·산업 장비 운송 전문 기업</title>
+      <title>대한카고크레인 | 중량물·건설자재·화물 운송 전문</title>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="최적화된 맞춤형 운송 솔루션과 안전하고 효율적인 작업을 통해 고객의 성공적인 프로젝트 완수를 지원합니다." />
