6ea522e9c6ffd87dfa5cdc95797b77b04424ec1d
Wix Remote Machine <support@wix.com>
2025-11-25 04:16:08 +0000

    Add favicon using provided image

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -6,6 +6,8 @@ export const Head = () => {
       <meta name="description" content="최적화된 맞춤형 운송 솔루션과 안전하고 효율적인 작업을 통해 고객의 성공적인 프로젝트 완수를 지원합니다." />
       <meta name="naver-site-verification" content="4775914a9f3ec89f8319d68d878db78083e33fc2" />
       <meta name="naver-site-verification" content="c11cba63c6d7d068d4ad3d7e96912b1392370252" />
+      {/* Favicon */}
+      <link rel="icon" type="image/png" href="https://static.wixstatic.com/media/6820d4_b8bb9e74216a45bda96bf7cfb92afadd~mv2.png" />
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
     </>
