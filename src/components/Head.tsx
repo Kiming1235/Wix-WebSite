0883247354d4f41be144b6fdf5a61870c00ab6a6
Wix Remote Machine <support@wix.com>
2025-11-25 04:31:48 +0000

    Add main keywords to site meta tags

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -5,6 +5,7 @@ export const Head = () => {
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="최적화된 맞춤형 운송 솔루션과 안전하고 효율적인 작업을 통해 고객의 성공적인 프로젝트 완수를 지원합니다." />
+      <meta name="keywords" content="대한카고크레인, 경산크레인, 대구크레인, 경주크레인, 구미크레인, 성주크레인, 중량물 운송, 산업 장비 운송" />
       <meta name="naver-site-verification" content="4775914a9f3ec89f8319d68d878db78083e33fc2" />
       <meta name="naver-site-verification" content="c11cba63c6d7d068d4ad3d7e96912b1392370252" />
       {/* Favicon */}
