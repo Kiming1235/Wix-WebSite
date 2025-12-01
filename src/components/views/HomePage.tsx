3a5b0560ff6e68a518ee0bf6b3a27c0318ded1ce
Wix Remote Machine <support@wix.com>
2025-12-01 00:59:40 +0000

    Set mobile gallery grid to 3 columns

--- src/components/views/HomePage.tsx
+++ src/components/views/HomePage.tsx
@@ -285,7 +285,7 @@ export default function HomePage() {
           </motion.div>
 
           {/* Grid Gallery */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-3 gap-4">
             {galleryImages.map((image, index) => (
               <motion.div
                 key={index}
