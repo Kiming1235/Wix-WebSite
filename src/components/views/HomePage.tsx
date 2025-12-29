c1b7fe204d33a2d41a9675b21cf3f1f2593c45e5
Wix Remote Machine <support@wix.com>
2025-12-29 06:16:54 +0000

    Restored to checkpoint c8f300da11edf5dc3f7c57b40f424ff25df4c6e5

--- src/components/views/HomePage.tsx
+++ src/components/views/HomePage.tsx
@@ -383,7 +383,7 @@ export default function HomePage() {
           </motion.div>
 
           {/* Grid Gallery */}
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-3">
+          <div className="grid grid-cols-4 lg:grid-cols-6 gap-3">
             {galleryImages.map((image, index) => (
               <motion.div
                 key={index}
